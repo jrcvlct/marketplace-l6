876a0acc608ada87f39a44d4f429f282c5f1e272
Josimar Junior <juniorcvlct17@gmail.com>
2022-04-12 11:58:12 -0400

    crud produtos e lojas

--- app/Product.php
+++ app/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'description', 'body', 'price', 'slug'];
+    
     public function store()
     {
         return $this->belongsTo(Store::class);
